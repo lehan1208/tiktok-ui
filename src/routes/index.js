00ae34565151a9a39568305326d1e57e0b7c9587
Han <101984174+HaanLee@users.noreply.github.com>
2022-05-24 21:56:25 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout'
+
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+const publicRoutes = [ // Không cần đăng nhập vẫn xem được
+
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+]
+const priveRoutes = [ // Không đăng nhập sẽ hướng đến trang log-in
+
+]
+
+export { publicRoutes, priveRoutes }
